88cb0e71c84e1f9865dd12d34cb059b656d361f1
geofflambeth <66445285+geofflambeth@users.noreply.github.com>
2021-02-07 12:40:39 -0700

    Update automation.sh
    Simplified automatic git merge method.

--- automation.sh
+++ automation.sh
@@ -2,10 +2,10 @@
 
 cd /Users/geofflambeth/OneDrive/GitHub/COVID-19app/
 git checkout automated
-git merge upstream/master
-
-git checkout automated
-git checkout --patch StableREADME README.md
+git merge --no-ff --no-commit upstream/master
+git reset HEAD README.md
+git checkout -- README.md
+git commit -m "Automatically merged upstream/master"
 
 cd /Users/geofflambeth/OneDrive/GitHub/COVID-19app/ShinyApp
 /usr/local/bin/Rscript Update.R
